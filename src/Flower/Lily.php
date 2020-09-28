ec3d1a0fdcb5d5ef984153c6eb1606e97f234730
vagrant <vagrant@homestead>
2020-09-28 09:01:48 +0000

    init && dev package

---
+++ src/Flower/Lily.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peiyongqing
+ * Date: 2020/9/28
+ * Time: 16:53
+ */
+
+namespace Flower\Lily;
+
+class Lily
+{
+    public function desc()
+    {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peiyongqing
+ * Date: 2020/9/28
+ * Time: 16:53
+ */
+
+namespace Flower\Rose;
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
